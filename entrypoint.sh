7e41897726b8ae024ac612dec8c2befd25e4852f
DinDing1 <119488409+DinDing1@users.noreply.github.com>
2025-05-02 21:31:58 +0800

    更新 entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e
 
-# 执行授权检查
-./auth_check.sh
+# 执行鉴权检查（阻塞式）
+if ! ./auth_check.sh ; then
+    echo -e "\033[31m[FATAL] 鉴权失败，容器终止\033[0m" >&2
+    exit 1
+fi
 
-# 如果授权失败，auth_check.sh 会退出并返回非零状态码，容器启动终止
-# 授权成功后，执行 CMD 中的命令
+# 鉴权通过后启动主服务
 exec "$@"
